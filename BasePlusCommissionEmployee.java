5b8b55b3576ec6e7b2144c3de1570c74b1849e04
AARELLANO4 <51343015+AARELLANO4@users.noreply.github.com>
2021-01-04 23:53:16 -0500

    edit file headers

--- BasePlusCommissionEmployee.java
+++ BasePlusCommissionEmployee.java
@@ -1,14 +1,4 @@
-/**********************************************
- * Workshop 3 
- * Course: JAC444 - WINTER 2020
- * Last Name: Arellano
- * First Name: Alexis
- * ID: 155816184
- * Section: NDD
- * This assignment represents my own work in accordance with Seneca Academic Policy.
- *  - AA
- * Date: Saturday October 10/2020
- * **********************************************/
+/* Alexis Arellano Saturday October 10/2020 */
 
 package ca.senecacollege.ict;
 
